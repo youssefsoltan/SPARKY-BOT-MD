8f0f7d03eea33632b804a41848a445858a95c967
Sparkymon777 <105107888+Sparkymon777@users.noreply.github.com>
2022-12-13 20:10:39 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
-npm start
-
-echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
