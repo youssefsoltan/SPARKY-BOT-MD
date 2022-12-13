022286ebf1a85521bb00b9b6de175e5146fae78d
Sparkymon777 <105107888+Sparkymon777@users.noreply.github.com>
2022-12-13 16:51:46 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+npm start
+
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
