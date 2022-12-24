faf0dceef60746c921a2bb097a60a9dfa54b5c57
youssefsoltan <97458904+youssefsoltan@users.noreply.github.com>
2022-12-24 16:17:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,29 +14,29 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['917012984396'] //ganti nomer lu
-global.ownername = "ASWIN SPARKY" //ganti nama lu
-global.developer = "ASWIN" //nama lu
-global.ytname = "YT: SPARKY MON 777" //ur yt chanel name
-global.socialm = "https://www.instagram.com/aswin_sparky.exe" //taroh sosialmedia lu kalo ga punya biarin aja
-global.sgc = "https://chat.whatsapp.com/JTUrdnOziKD44ScoBoggmh" //ganti group lu
-global.location = "Kerala,TVM,Varkala" //ganti lokasi lu
+global.vcardowner = ['201227179105'] //ganti nomer lu
+global.ownername = "YoUsSef Al SoLtAn" //ganti nama lu
+global.developer = "YoUsSef Al SoLtAn" //nama lu
+global.ytname = "YT: YOUSSEF-ALSOLTN" //ur yt chanel name
+global.socialm = "https://www.instagram.com/1y._.o2.exe" //taroh sosialmedia lu kalo ga punya biarin aja
+global.sgc = "https://www.instagram.com/1y._.o2" //ganti group lu
+global.location = "Egypt,cairo,kafr el dwar" //ganti lokasi lu
 
 //bot bomdy 
-global.owner = ['917012984396'] //ur number
-global.ownernomer = "917012984396" //ur number
-global.ownertag = '917012984396' //ur tag number
-global.premium = ['917012984396'] //ur premium number
-global.botname = 'SPARKY MD+' //ur bot name
+global.owner = ['201227179105'] //ur number
+global.ownernomer = "201227179105" //ur number
+global.ownertag = '201227179105' //ur tag number
+global.premium = ['201227179105'] //ur premium number
+global.botname = 'AL SOLTAN MD+' //ur bot name
 global.ownername = "ASWIN SPARKY"
-global.linkz = "https://youtube.com/@sparkymon777" //your theme url which will be displayed on whatsapp
-global.dana = "917012984396"
-global.pulsa = "917012984396"
-global.websitex = "https://youtube.com/@sparkymon777" //ur website to be displayed
+global.linkz = "https://youtube.com/@YOUSSEF-ALSOLTN" //your theme url which will be displayed on whatsapp
+global.dana = "201227179105"
+global.pulsa = "201227179105"
+global.websitex = "https://youtube.com/@YOUSSEF-ALSOLTN" //ur website to be displayed
 global.websitex1 = "https://sparkymodswa.blogspot.com"
-global.botscript = 'https://github.com/Sparkymon777/SPARKY-BOT-MD' //script link
+global.botscript = 'https://youtube.com/@YOUSSEF-ALSOLTN' //script link
 global.themeemoji = "👀" //ur theme emoji
-global.packname = "S P A R K Y" //ur sticker watermark packname
+global.packname = "YoUsSef Al SoLtAn" //ur sticker watermark packname
 global.author = "ASWIN" //ur sticker watermark author
 global.wm = "S P A R K Y" //ur watermark
 
